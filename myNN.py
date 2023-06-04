f0229ddebfa633ed68ecfcbc888e8affed9accc2
Nam Nguyen <namnguyen.nnh99@gmail.com>
2023-06-04 03:33:13 +0100

    Added general layer attr. for every layer and fixed NN.summary

--- myNN.py
+++ myNN.py
@@ -11,26 +11,30 @@ from tabulate import tabulate
 
 # layers:
 class ConvolutionalLayer:
-    def __init__(self, input_shape=None, no_of_filters=1, filter_size=3, stride=1, pad=0, pad_value=0):
-        self.name = 'Convolutional'
-        self.input_shape = input_shape
-        self.channels = input_shape[2]
-        self.no_of_filters = no_of_filters
+    def __init__(self, input_shape=None, filters=1, filter_size=3, stride=1, pad=0, pad_value=0):
+        # specific layer attributes:
+        self.filters = filters
         self.filter_size = filter_size
         self.stride = stride
         self.pad = pad
         self.pad_value = pad_value
-        # initialise the set of filters:
-        self.filters = np.random.randn(filter_size, filter_size, input_shape[-1], no_of_filters) * 0.01
-        self.filter_volume = filter_size*filter_size*input_shape[-1]
-        # determine the size of the output feature map:
+        self.channels = input_shape[2]
+        # initialise the set of filters and compute each filter's volume:
+        self.f = np.random.randn(filter_size, filter_size, input_shape[2], filters) * 0.01
+        self.filter_volume = filter_size*filter_size*input_shape[2]
+        # compute the size of the output feature map:
         self.output_size = int((input_shape[0] - filter_size + 2*pad)/stride) + 1
-        # keeping track of each input
+        # input cache:
         self.input = None
+        # general layer attributes:
+        self.name = 'Convolutional'
+        self.input_shape = input_shape
+        self.output_shape = tuple((self.output_size, self.output_size, self.filters))
+        self.trainable_params = np.product(self.f.shape)
         '''
         To follow along the code for this layer, we use an example of:
         - input_shape = (32,32,3)
-        - no_of_filters = 16,
+        - filters = 16,
         - filter_size = 5,
         - stride = 3, pad = 3
         
@@ -48,7 +52,7 @@ class ConvolutionalLayer:
             image = np.pad(image, [(self.pad,self.pad), (self.pad,self.pad), (0,0)], 
                            'constant', constant_values=self.pad_value)
         
-        # store image to later backprop:
+        # store image in cache to later backprop:
         self.input = image # self.input now has shape (38,38,3)
 
         # create a windowed view of the image (i.e. convolution operation), each window being the shape of the filter (5,5,3).
@@ -69,10 +73,10 @@ class ConvolutionalLayer:
                                               self.filter_volume).reshape(self.output_size**2,
                                                                           self.filter_volume)
         
-        flat_filters = self.filters.reshape(self.filter_volume, self.no_of_filters)
+        flat_filters = self.f.reshape(self.filter_volume, self.filters)
         output = np.dot(flat_windows,flat_filters).reshape(self.output_size,
                                                            self.output_size,
-                                                           self.no_of_filters)
+                                                           self.filters)
         return output
     
     def backprop(self, dL_dout, learn_rate):
@@ -89,7 +93,7 @@ class ConvolutionalLayer:
         # initialise the expanded output gradient matrix:
         expanded_dL_dout = np.zeros((self.output_size*self.stride - (self.stride - 1),
                                      self.output_size*self.stride - (self.stride - 1),
-                                     self.no_of_filters), dtype=dL_dout.dtype)
+                                     self.filters), dtype=dL_dout.dtype)
         
         # replace values at stride intervals with dL_dout to complete the expanded matrix:
         expanded_dL_dout[::self.stride,::self.stride,:] = dL_dout
@@ -107,15 +111,15 @@ class ConvolutionalLayer:
         # windowed_input shape: (5,5,34,34,3)
         # transpose to get shape (5,5,3,34,34) then flatten to 2D for matrix multiplication:
         flat_windows = windowed_input.transpose(0,1,4,2,3).reshape(self.filter_volume, x**2)
-        flat_dL_dout = expanded_dL_dout.reshape(x**2,self.no_of_filters)
-        dL_df = np.dot(flat_windows, flat_dL_dout).reshape(self.filters.shape)
+        flat_dL_dout = expanded_dL_dout.reshape(x**2,self.filters)
+        dL_df = np.dot(flat_windows, flat_dL_dout).reshape(self.f.shape)
 
         '''
         2. The input gradient matrix can be mapped by performing FULL convolution on expanded_dL_dout, 
-        using a flipped self.filters as filters
+        using a flipped self.f as filters
         '''
         # flipping the filter on the first 2 dimensions:
-        flipped_filters = np.flip(self.filters, (0,1))
+        flipped_filters = np.flip(self.f, (0,1))
 
         # pad expanded_dL_dout with enough 0s on the first 2 dimensions:
         padded_dL_dout = np.pad(expanded_dL_dout, [(self.filter_size-1,self.filter_size-1),
@@ -125,7 +129,7 @@ class ConvolutionalLayer:
         windowed_dL_dout = np.lib.stride_tricks.as_strided(padded_dL_dout,
                                                          shape=(self.input.shape[0], self.input.shape[0],
                                                                 self.filter_size, self.filter_size,
-                                                                self.no_of_filters),
+                                                                self.filters),
                                                          strides=(padded_dL_dout.strides[0],
                                                                   padded_dL_dout.strides[1],
                                                                   padded_dL_dout.strides[0],
@@ -134,24 +138,28 @@ class ConvolutionalLayer:
         
         # windowed_dL_dout shape: (38,38,5,5,16), flattened to (1444,400)
         # flipped_filters shape (5,5,3,16), transposed to (5,5,16,3) and flattened to (400, 3): 
-        flat_dL_dout_windows = windowed_dL_dout.reshape(self.input.shape[0]**2, (self.filter_size**2)*self.no_of_filters)
-        flat_filters = flipped_filters.transpose(0,1,3,2).reshape((self.filter_size**2)*self.no_of_filters,self.channels)
+        flat_dL_dout_windows = windowed_dL_dout.reshape(self.input.shape[0]**2, (self.filter_size**2)*self.filters)
+        flat_filters = flipped_filters.transpose(0,1,3,2).reshape((self.filter_size**2)*self.filters,self.channels)
         dL_din = np.dot(flat_dL_dout_windows,flat_filters).reshape(self.input.shape)
 
         # finally, we update the filters and return dL_din:
-        self.filters -= dL_df * learn_rate
+        self.f -= dL_df * learn_rate
         return dL_din
 
 class MaxPoolingLayer:
     def __init__(self, input_shape=None, pool_size=2):
-        self.name = 'MaxPooling'
-        self.input_shape = input_shape
+        # specific layer attributes:
         self.pool_size = pool_size
         self.stride = pool_size
-        # compute output size:
         self.output_size = int((input_shape[0] - pool_size)/pool_size) + 1
         self.channels = input_shape[2]
+        # cache:
         self.pools = None
+        # general layer attributes:
+        self.name = 'MaxPooling'
+        self.input_shape = input_shape
+        self.output_shape = tuple((self.output_size, self.output_size, self.channels))
+        self.trainable_params = 0
         '''
         Following the example from the previous layer:
         - input_shape = (12,12,16) i.e. ConvolutionalLayer output shape
@@ -171,7 +179,7 @@ class MaxPoolingLayer:
                                                                    sample.strides[0],
                                                                    sample.strides[1],
                                                                    sample.strides[2]))
-        # windowed_sample shape: (6,6,2,2,16), store to later use in backprop:
+        # windowed_sample shape: (6,6,2,2,16), store in cache to later use in backprop:
         self.pools = windowed_sample
         # return the one max value from each 2x2 window (axes 2 and 3) --> shape (6,6,16):
         return np.amax(windowed_sample, axis=(2,3))
@@ -207,15 +215,21 @@ class MaxPoolingLayer:
         return windowed_dL_din.transpose(0,2,1,3,4).reshape(self.input_shape)
 
 class ActivationLayer:
-    def __init__(self, fn: Literal['relu','leakyrelu','sigmoid','softmax']='relu', alpha=0.001):
-        self.function = fn
+    def __init__(self, function: Literal['relu','leakyrelu','sigmoid','softmax']='relu', alpha=0.001):
+        # specific layer attributes:
+        self.function = function
+        self.alpha = alpha # for leaky relu only
+        # cache:
         self.input = None
         self.output = None
-        self.alpha = alpha # for leaky relu only
-        self.name = str(fn) + ' activation'
+        # general layer attributes
+        self.name = 'Activation: '+str(function)
+        self.input_shape = '_' #
+        self.output_shape = self.input_shape
+        self.trainable_params = 0
         '''
-        For this layer, input and output shape are the same, which can be any shape for most fn, except for softmax.
-        For softmax, input and output shape would be (n,1), where for the cifar-10 dataset n=10
+        For this layer, there's no need to keep track of input and output shape since they're not needed for
+        both forward and back prop, and they're always equal to each other.
         '''
     def forwardprop(self, input_arr):
         # store input to later derive:
@@ -259,12 +273,14 @@ class ActivationLayer:
         return dL_din
      
 class FlattenLayer:
-    def __init__(self):
-        self.input_shape = None
+    def __init__(self, input_shape):
+        # general layer attributes:
         self.name = 'Flatten'
+        self.input_shape = input_shape
+        self.output_shape = tuple((np.product(input_shape),1))
+        self.trainable_params = 0
     
     def forwardprop(self, input_arr):
-        self.input_shape = input_arr.shape
         # flatten to rows:
         return input_arr.reshape(-1,1)
     
@@ -274,19 +290,23 @@ class FlattenLayer:
         
 class DenseLayer: 
     def __init__(self, units_in, units_out, initial_Wvar=0.1):
-        self.name = 'Dense'
-        # by passing in initial_Wvar, it is possible to choose the weights initialisation strategy: kaiming, xavier, random, etc.
+        # specific layer attributes:
         self.weights = np.random.randn(units_out, units_in) * np.sqrt(initial_Wvar)
         self.biases = np.zeros(shape=(units_out,1))
-        self.units_in = units_in
-        self.units_out = units_out
+        # cache:
         self.input = None
+        # general layer attributes:
+        self.name = 'Dense'
+        self.input_shape = tuple((units_in,1))
+        self.output_shape = tuple((units_out,1))
+        self.trainable_params = np.product(self.weights.shape) + units_out # number of biases = units_out
     '''
     Dense a.k.a fully-connected layer:
     - units_in and units_out are the number of input and output neurons for the layer, respectively
-    - e.g.: units_in=100 and units_out=10, weights will have shape (10,100) and biases (10,1)
+    - e.g.: with units_in = 100 and units_out = 10, weights will have shape (10,100) and biases (10,1)
     - initial_Wvar specifies the variance for the initial weights distribution. Initializing each layer's weights appropriately could 
-    help the network be more stable and converge faster.
+    help the network learn more stably and converge faster. Well known initialisation strategies include He, Xavier or random, which 
+    can be implemented by passing in the appropriate initial_Wvar
     '''
     def forwardprop(self, input_arr):
         # store input to later backprop:
@@ -306,10 +326,11 @@ class DenseLayer:
 class NN:
     def __init__(self):
         self.layers = []
-    
+        self.previous_output_shape = None
+
     def add(self, layer):
         self.layers.append(layer)
-    
+
     def forwardpass(self, image):
         for layer in self.layers:
             image = layer.forwardprop(image)
@@ -320,41 +341,21 @@ class NN:
             dL_dout = layer.backprop(dL_dout, learn_rate)
     
     def summary(self):
-        table = [['Layer','Input Shape','Output Shape','Trainable Parameters']]
-        previous_output = None
+        table = [['LAYER NAME','INPUT SHAPE','OUTPUT SHAPE ','TRAINABLE PARAMS']]
+        total_params = 0
         for layer in (self.layers):
             lst = []
             lst.append(layer.name)
-            if layer.name == 'Convolutional': 
-                lst.append(layer.input_shape)
-                output_shape = tuple(layer.output_size, layer.output_size, layer.no_of_filters)
-                lst.append(output_shape)
-                lst.append(layer.filter_volume*layer.no_of_filter)
-            if layer.name == 'MaxPooling':
-                lst.append(layer.input_shape)
-                output_shape = tuple(layer.output_size, layer.output_size, layer.channels)
-                lst.append(output_shape)
-                lst.append(0)
-            if 'activation' in layer.name:
-                lst.append('_')
-                output_shape = previous_output
-                lst.append('_')
-                lst.append(0)
-            if layer.name == 'Flatten':
-                lst.append(previous_output)
-                output_shape = np.product(previous_output)
-                lst.append(output_shape)
-                lst.append(0)
-            if layer.name == 'Dense':
-                lst.append(tuple(layer.units_in,1))
-                output_shape = tuple(layer.units_out,1)
-                lst.append(output_shape)
-                lst.append(layer.units_in*layer.units_out + layer.units_out)
-            # save layer output shape for next layer's access:
-            previous_output = output_shape
+            lst.append(layer.input_shape)
+            lst.append(layer.output_shape)
+            lst.append(layer.trainable_params)
+            # update total params:
+            total_params += layer.trainable_params
+            # update table:
             table.append(lst)
+        table.append(['TOTAL','','',total_params])
         print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
-  
+
     def train(self, x_train, y_train, epochs, learn_rate):
         y_size = np.max(y_train)+1 # vector size of one-hot encoded y
         
